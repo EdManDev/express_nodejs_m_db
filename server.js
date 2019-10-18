eb557bb9ef4d2965037f1b6aa5bf13e0f20698ae
edmandev <ed.manigat@gmail.com>
2019-10-18 16:09:01 -0400

    add

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+// add mdb
+const mongoose = require("mongoose");
+
+// mdb config
+const db = require("./config/keys").mongoURI;
+
+// connect to mdb from mongoose
+mongoose
+	.connect(db)
+	.then(() => console.log("mongoDB connected✅"))
+	.catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 1000;
