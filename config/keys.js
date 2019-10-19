750d8f6bb86d8a5e410f740e1287b969b8dec530
edmandev <ed.manigat@gmail.com>
2019-10-18 20:23:50 -0400

    go

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,12 @@
 module.exports = {
 	mongoURI:
+		"mongodb+srv://admin123:admin123@twitter-app-ul2jp.mongodb.net/test?retryWrites=true&w=majority"
 };
+
+// if (process.env.NODE_ENV === "production") {
+// 	// mongodb+srv://scriptomania:group23@script-6wmwk.mongodb.net/test?retryWrites=true
+// 	module.exports = {
+// 		mongoURI:
+// 			"mongodb+srv://scriptomania:group23@script-6wmwk.mongodb.net/test?retryWrites=true"
+// 	};
+// }
